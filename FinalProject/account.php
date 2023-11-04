a8f5dc5ea1815e7a23c04a6090f547767b8a508d
justin <justinpng73@gmail.com>
2023-11-05 06:03:49 +0800

    justin update v0.03

---
+++ FinalProject/account.php
@@ -0,0 +1,375 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "shoeshop";
+
+$conn = new mysqli($hostname, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$registrationSuccess = false;
+$registrationMessage = "";
+$loginSuccess = false;
+$loginMessage = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['login-username']) && isset($_POST['login-password'])) {
+        // Handle login form submission
+        $loginInput = $_POST['login-username'];
+        $loginPassword = $_POST['login-password'];
+    
+        // Query the database to check the credentials
+        $loginQuery = "SELECT username, email, password FROM users WHERE username = ? OR email = ?";
+        $loginStmt = $conn->prepare($loginQuery);
+        $loginStmt->bind_param("ss", $loginInput, $loginInput); // Check both username and email
+        $loginStmt->execute();
+        $loginStmt->bind_result($dbUsername, $dbEmail, $dbPassword);
+    
+        if ($loginStmt->fetch()) {
+            if (password_verify($loginPassword, $dbPassword)) {
+                // Login successful
+                $loginSuccess = true;
+                $loginMessage = "Login successful.";
+            } else {
+                // Password doesn't match
+                $loginMessage = "Invalid password.";
+            }
+        } else {
+            // Username or email not found in the database
+            $loginMessage = "User not found.";
+        }
+    
+        $loginStmt->close();
+    } else {
+        // Get form data for registration
+        $first_name = $_POST['first-name'];
+        $last_name = $_POST['last-name'];
+        $username = $_POST['create-username'];
+        $email = $_POST['create-email'];
+        $password = $_POST['create-password'];
+    
+        // Check for duplicate username or email
+        $checkQuery = "SELECT username, email FROM users WHERE username = ? OR email = ?";
+        $checkStmt = $conn->prepare($checkQuery);
+        $checkStmt->bind_param("ss", $username, $email);
+        $checkStmt->execute();
+    
+        $checkStmt->bind_result($foundUsername, $foundEmail);
+    
+        $duplicateFound = false;
+    
+        while ($checkStmt->fetch()) {
+            if ($foundUsername === $username) {
+                $registrationMessage = "Error: Username already exists.";
+                $duplicateFound = true;
+                break; // No need to continue checking
+            }
+            if ($foundEmail === $email) {
+                $registrationMessage = "Error: Email already registered.";
+                $duplicateFound = true;
+                break; // No need to continue checking
+            }
+        }
+    
+        $checkStmt->close();
+    
+        if (!$duplicateFound) {
+            // No duplicate found, proceed with the registration
+            // Assuming $password contains the user's password
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    
+            // Insert the hashed password into the database
+            $sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("sss", $username, $email, $hashedPassword);
+    
+            if ($stmt->execute()) {
+                // Registration successful
+                $registrationSuccess = true;
+                $registrationMessage = "Account created successfully.";
+            } else {
+                // Error occurred during data insertion
+                $registrationMessage = "Error creating the account.";
+            }
+    
+            $stmt->close();
+        }
+    }
+}
+
+$conn->close();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shoe Shop - Product Listing</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,600&family=Lato:wght@700&display=swap" rel="stylesheet">
+    <script src="scripts.js" defer></script>
+    <script type="text/JavaScript">
+        var message = "Current Promotions Latest News Get it Here";
+        var space = " ";
+        var position = 0;
+
+        function scroller() {
+            var newtext = space + message.substring(position, message.length) + space + message.substring(0, position);
+            var td = document.getElementById("tabledata");
+            td.firstChild.nodeValue = newtext;
+            position++;
+            if (position > message.length) {
+                position = 0;
+            }
+            setTimeout(scroller, 200);
+        }
+    </script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/15.4.0/nouislider.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/15.4.0/nouislider.min.js"></script>
+
+    <script>
+        var registrationSuccess = <?php echo json_encode($registrationSuccess); ?>;
+        var registrationMessage = <?php echo json_encode($registrationMessage); ?>;
+        var loginSuccess = <?php echo json_encode($loginSuccess); ?>;
+        var loginMessage = <?php echo json_encode($loginMessage); ?>;
+
+        function showMessage() {
+            if (registrationSuccess) {
+                alert("Account created successfully.");
+            } else if (registrationMessage) {
+                alert(registrationMessage);
+            }
+            if (loginSuccess) {
+                alert("Login successful.");
+            } else if (loginMessage) {
+                alert(loginMessage);
+            }
+        }
+
+        showMessage(); // Call the function on page load
+    
+    </script>
+
+
+    <style>
+        .accounts {
+            display: flex;
+            justify-content: center;
+            align-items: flex-start;
+            margin-top: 80px;
+            margin-bottom: 180px;
+        }
+
+        .accounts-container {
+            display: flex;
+            background-color: #f9f9f9;
+            justify-content: center;
+            align-items: flex-start;
+            border: 1px solid #ccc; /* Add border to the container */
+            width: 66%; /* Set width to 66% of the page */
+            margin: 0 auto; /* Center the container horizontally */
+            padding-bottom: 20px;
+        }
+
+        .accounts-form {
+            width: 100%; /* Make each form fill the container width */
+            text-align: left;
+            height: 710px;
+            padding-left: 80px;
+            padding-top: 40px;
+            font-size: 150%;
+        }
+
+        .accounts-form h2 {
+            margin: 0;
+        }
+
+        .accounts-form label {
+            display: block;
+            font-size: 80%;
+            margin-top: 20px;
+            margin-bottom: 10px;
+        }
+
+        .accounts-form input {
+            display: block;
+            width: 80%;
+            padding: 8px;
+        }
+
+        .accounts-form button {
+            padding: 14px;
+            background-color: #000000;
+            color: #fff;
+            border: 1px solid #000000;
+            cursor: pointer;
+            margin-top: 30px;
+            width: 80%;
+            font-family: 'DM Sans', sans-serif;
+            font-weight: 600;
+        }
+
+        .accounts-form button:hover {
+            background-color: #0056b3;
+        }
+    </style>
+</head>
+
+<body onload="scroller();">
+    <div class="promo">
+        <table border="0">
+            <tr>
+                <td id="tabledata">Current Promotion</td>
+            </tr>
+        </table>
+    </div>
+    <nav class="navbar">
+        <div class="navleft">
+            <span>Link 1</span>
+            <span>Link 2</span>
+            <span>Link 3</span>
+            <span>Link 4</span>
+        </div>
+        <div class="navcenter">
+            <span>Logo</span>
+        </div>
+        <div class="navright">
+            <span>Link 1</span>
+            <span>Link 2</span>
+            <span>Link 3</span>
+            <span>Search</span>
+        </div>
+    </nav>
+
+    <div class="accounts">
+        <div class="accounts-container">
+            <div class="accounts-form">
+                <h2>Login</h2>
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label for="login-username">Username or Email:</label>
+                        <input type="text" id="login-username" name="login-username" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="login-password">Password:</label>
+                        <input type="password" id="login-password" name="login-password" required>
+                    </div>
+
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+
+            <div class="accounts-form">
+                <h2>Create Account</h2>
+                <form action="" method="post" onsubmit="return validateForm();">
+                    <div class="form-group">
+                        <label for="first-name">First Name:</label>
+                        <input type="text" id="first-name" name="first-name" pattern="[A-Za-z]+" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="last-name">Last Name:</label>
+                        <input type="text" id="last-name" name="last-name" pattern="[A-Za-z]+" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="create-username">Username:</label>
+                        <input type="text" id="create-username" name="create-username" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="create-email">Email:</label>
+                        <input type="email" id="create-email" name="create-email" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="create-password">Password:</label>
+                        <input type="password" id="create-password" name="create-password" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="confirm-password">Confirm Password:</label>
+                        <input type="password" id="confirm-password" name="confirm-password" required>
+                    </div>
+
+                    <button type="submit">Create Account</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="footerupper">
+            <div class="sitemap">
+                <a style="font-size: 25px; text-decoration: underline;"> <strong>Quick Directory </strong> </a> <br>
+                <table class="sitemaplinks">
+                    <tr>
+                        <td> <a> Link 1</a> </td>
+                        <td> <a> Link 2</a> </td>
+                    </tr>
+                    <tr>
+                        <td> <a> Link 1</a> </td>
+                        <td> <a> Link 2</a> </td>
+                    </tr>
+                </table>
+            </div>
+            <div class="socialmedia">
+                <a>facebook</a>
+                <a>instagram</a>
+                <a>youtube</a>
+            </div>
+        </div>
+        <div class="copyright">
+            <a> 2023 ShoeShoe Singapore Ltd</a>
+        </div>
+    </div>
+
+    <script>
+
+        function validateForm() {
+            if (!checkEmail()) {
+                alert("Invalid email format. Please correct it.");
+                return false; // Prevent form submission
+            }
+
+            if (!validatePassword()) {
+                return false; // Prevent form submission
+            }
+
+            // Add other form validation logic here if needed
+            return true; // Allow form submission if all checks pass
+        }
+
+        function checkEmail() {
+            var email = document.getElementById("create-email").value;
+
+            if (email.trim() === "") {
+                alert("Please fill up your email.");
+                return false;
+            } else if (!/^[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)) {
+                return false;
+            }
+
+            return true;
+        }
+
+        function validatePassword() {
+            var password = document.getElementById("create-password").value;
+            var confirmPassword = document.getElementById("confirm-password").value;
+
+            if (password !== confirmPassword) {
+                alert("Passwords do not match.");
+                return false; // Prevent form submission
+            }
+
+            return true;
+        }
+    </script>
+
+</body>
+</html>
--- FinalProject/product_list.php
+++ FinalProject/product_list.php
@@ -13,14 +13,17 @@ if ($conn->connect_error) {
 // SQL query to select unique colors and brands for filtering
 $filterSql = "SELECT DISTINCT colorway FROM inventory";
 $filter2Sql = "SELECT DISTINCT brand FROM inventory";
+$filter3Sql = "SHOW COLUMNS FROM inventory WHERE Field LIKE 'US%'";
 
 // Execute the filter SQL query
 $filterResult = $conn->query($filterSql);
 $filterResult2 = $conn->query($filter2Sql);
+$filterResult3 = $conn->query($filter3Sql);
 
 // Initialize arrays to store unique colors and brands for filtering
 $colors = array();
 $brands = array();
+$sizes = array();
 
 if ($filterResult->num_rows > 0) {
     while ($row = $filterResult->fetch_assoc()) {
@@ -34,9 +37,17 @@ if ($filterResult2->num_rows > 0) {
     }
 }
 
+if ($filterResult3->num_rows > 0) {
+    while ($row = $filterResult3->fetch_assoc()) {
+        // Extract the size from the column name and store it
+        $sizeColumns[] = str_replace('US ', '', $row['Field']);
+    }
+}
+
+
 if (isset($_GET['brand'])) {
     $selectedBrand = $_GET['brand'];
-    $productsSql = "SELECT image_data, product_name, price, release_date, colorway, brand FROM inventory WHERE brand = '$selectedBrand'";
+    $productsSql = "SELECT id,image_data, product_name, price, release_date, colorway, brand FROM inventory WHERE brand = '$selectedBrand'";
 
     // Execute the products SQL query
     $productsResult = $conn->query($productsSql);
@@ -53,7 +64,7 @@ if (isset($_GET['brand'])) {
 }  else {
 
 // SQL query to select all fields for displaying products
-$productsSql = "SELECT product_page_url, image_data, product_name, price, release_date, colorway, brand FROM inventory";
+$productsSql = "SELECT id,image_data, product_name, price, release_date, colorway, brand FROM inventory";
 
 // Execute the products SQL query
 $productsResult = $conn->query($productsSql);
@@ -148,10 +159,36 @@ $conn->close();
                     </div>
                     <div class="checkbox-form">
                         <form>
-                            <label for="size-7"><input type="checkbox" id="size-7" class="size-filter" value="7">Size 7</label>
-                            <label for="size-8"><input type="checkbox" id="size-8" class="size-filter" value="8">Size 8</label>
-                            <label for="size-9"><input type="checkbox" id="size-9" class="size-filter" value="9">Size 9</label>
-                            <!-- Add more size checkboxes as needed -->
+                            <?php foreach ($sizeColumns as $size): ?>
+                                <?php
+                                // Count the number of rows with available size and get the product IDs
+                                $sizeCount = 0;
+                                $availableProductIds = array();
+                                $conn = new mysqli($hostname, $username, $password, $database);
+
+                                if ($conn->connect_error) {
+                                    die("Connection failed: " . $conn->connect_error);
+                                }
+
+                                $sizeQuery = "SELECT COUNT(*) as count, GROUP_CONCAT(id) as product_ids FROM inventory WHERE `US $size` > 0";
+                                $sizeResult = $conn->query($sizeQuery);
+
+                                if ($sizeResult->num_rows > 0) {
+                                    $sizeRow = $sizeResult->fetch_assoc();
+                                    $sizeCount = $sizeRow['count'];
+                                    $productIds = $sizeRow['product_ids'];
+                                    $availableProductIds[$size] = explode(',', $productIds);
+                                }
+
+                                $conn->close();
+                                ?>
+
+                                <label for="size-<?php echo $size; ?>">
+                                    <input type="checkbox" id="size-<?php echo $size; ?>" class="size-filter" value="<?php echo $size; ?>" data-product-ids="<?php echo implode(',', $availableProductIds[$size]); ?>">
+                                    Size <?php echo $size; ?> <span style="font-weight: normal; color: #B0B0B0;"> (<?php echo $sizeCount; ?>) </span>
+                                    <!-- <span style="font-weight: normal; color: #B0B0B0;"> IDs: <?php echo implode(',', $availableProductIds[$size]); ?></span> -->
+                                </label>
+                            <?php endforeach; ?>
                         </form>
                     </div>
                 </div>
@@ -197,7 +234,11 @@ $conn->close();
             </div>
 
             <hr class="dropdown-divider">
-                                
+
+            
+            <!-- <button id="clear-filters">Clear Filters</button> -->
+            
+            
             <!-- <div class="price-range-filter">
                 <label for="price-range">Price Range</label>
                 <div id="price-range"></div>
@@ -217,35 +258,61 @@ $conn->close();
                 <div class="product_listing_total_products">
                     Products:
                 </div>
-                <div class="product_listing_sorting">
+                <!-- <div class="product_listing_sorting">
                     <select id="sort-select">
                         <option value="default" disabled selected>Sort By:</option>
-                        <option value="popularity">Popularity</option>
                         <option value="highest-price">Highest Price</option>
                         <option value="lowest-price">Lowest Price</option>
                         <option value="new-arrivals">New Arrivals</option>
                     </select>
-                </div>
+                </div> -->
             </div>
             
             <div class="product_listing_grids">
                 <div class="product_listing_grid">
                     <?php foreach ($products as $product): ?>
-                        <div class="product_listing_product" data-colorway="<?php echo $product['colorway']; ?>" data-brand="<?php echo $product['brand']; ?>">
+                        <div class="product_listing_product" data-id="<?php echo $product['id']; ?>" data-colorway="<?php echo $product['colorway']; ?>" data-brand="<?php echo $product['brand']; ?>">
                             <a href="product_page.php?product_name=<?php echo urlencode($product['product_name']); ?>&colorway=<?php echo urlencode($product['colorway']); ?>">
                                 <img src="data:image/jpeg;base64,<?php echo base64_encode($product['image_data']); ?>" alt="<?php echo $product['product_name']; ?>">
                                 <p><?php echo $product['product_name']; ?></p>
                                 <p>$<?php echo $product['price']; ?></p>
+                                <!-- <p>ID: <?php echo $product['id']; ?></p> -->
                             </a>
                         </div>
                     <?php endforeach; ?>
-                    
                 </div>
             </div>
             
         </div>
 
 
+    </div>
+
+    <div class="footer">
+        <div class="footerupper">
+            <div class="sitemap">
+                <a style="font-size: 25px; text-decoration: underline;"> <strong>Quick Directory </strong> </a> <br>
+                <table class = sitemaplinks>
+                    <tr>
+                        <td> <a> Link 1</a> </td>
+                        <td> <a> Link 2</a> </td>
+                    </tr>
+                    <tr>
+                        <td> <a> Link 1</a> </td>
+                        <td> <a> Link 2</a> </td>
+                    </tr>
+                </table>
+                    
+            </div>
+            <div class="socialmedia">
+                <a>facebook</a>
+                <a>instagram</a>
+                <a>youtube</a>
+            </div>
+        </div>
+        <div class="copyright">
+            <a> 2023 ShoeShoe Singapore Ltd</a>
+        </div>
     </div>
 
 
@@ -272,110 +339,99 @@ dropdownBars.forEach((dropdownBar, index) => {
 });
 
 // Filtering
-// Get all brand and color checkboxes
+// Get all brand, color, and size checkboxes
 const brandCheckboxes = document.querySelectorAll('.brand-filter input[type="checkbox"]');
-const colorCheckboxes = document.querySelectorAll('.color-filter input[type="checkbox"]');
+  const colorCheckboxes = document.querySelectorAll('.color-filter input[type="checkbox"]');
+  const sizeCheckboxes = document.querySelectorAll('.size-filter input[type="checkbox"]');
 
-// Add an event listener to each checkbox
-brandCheckboxes.forEach(function (checkbox) {
-  checkbox.addEventListener('change', function () {
-    filterProducts();
+  // Add event listeners to all checkboxes
+  brandCheckboxes.forEach(function (checkbox) {
+    checkbox.addEventListener('change', function () {
+      filterProducts();
+    });
   });
-});
 
-colorCheckboxes.forEach(function (checkbox) {
-  checkbox.addEventListener('change', function () {
-    filterProducts();
+  colorCheckboxes.forEach(function (checkbox) {
+    checkbox.addEventListener('change', function () {
+      filterProducts();
+    });
   });
-});
 
-// Function to filter products by brand and color
-function filterProducts() {
-  // Get all selected brand checkboxes
-  const selectedBrands = Array.from(brandCheckboxes)
-    .filter(checkbox => checkbox.checked)
-    .map(checkbox => checkbox.value);
+  sizeCheckboxes.forEach(function (checkbox) {
+    checkbox.addEventListener('change', function () {
+      filterProducts();
+    });
+  });
 
-  // Get all selected color checkboxes
-  const selectedColors = Array.from(colorCheckboxes)
-    .filter(checkbox => checkbox.checked)
-    .map(checkbox => checkbox.value);
+  // Function to filter products by brand, color, and size
+  function filterProducts() {
+    // Get all selected brand checkboxes
+    const selectedBrands = Array.from(brandCheckboxes)
+      .filter(checkbox => checkbox.checked)
+      .map(checkbox => checkbox.value);
 
-  // Get all product containers
-  const productContainers = document.querySelectorAll('.product_listing_product');
+    // Get all selected color checkboxes
+    const selectedColors = Array.from(colorCheckboxes)
+      .filter(checkbox => checkbox.checked)
+      .map(checkbox => checkbox.value);
 
-  // Iterate through product containers and filter based on selected brands and colors
-  productContainers.forEach(function (container) {
-    const brand = container.getAttribute('data-brand');
-    const colorway = container.getAttribute('data-colorway');
+    // Get all selected size checkboxes
+    const selectedSizes = Array.from(sizeCheckboxes)
+      .filter(checkbox => checkbox.checked)
+      .map(checkbox => checkbox.getAttribute('data-product-ids').split(','))
+      .flat();
 
-    if (
-      (selectedBrands.length === 0 || selectedBrands.includes(brand)) &&
-      (selectedColors.length === 0 || selectedColors.includes(colorway))
-    ) {
-      container.style.display = 'block';
-    } else {
-      container.style.display = 'none';
+    // Get all product containers
+    const productContainers = document.querySelectorAll('.product_listing_product');
+
+    // Iterate through product containers and filter based on selected brands, colors, and sizes
+    productContainers.forEach(function (container) {
+      const brand = container.getAttribute('data-brand');
+      const colorway = container.getAttribute('data-colorway');
+      const productID = container.getAttribute('data-id');
+
+      if (
+        (selectedBrands.length === 0 || selectedBrands.includes(brand)) &&
+        (selectedColors.length === 0 || selectedColors.includes(colorway)) &&
+        (selectedSizes.length === 0 || selectedSizes.includes(productID))
+      ) {
+        container.style.display = 'block';
+      } else {
+        container.style.display = 'none';
+      }
+    });
+
+    
+  }
+
+  // Call the filterProducts function to initialize the filtering
+  filterProducts();
+
+  // Function to clear all selected checkboxes
+  function clearFilters() {
+        brandCheckboxes.forEach(function (checkbox) {
+            checkbox.checked = false;
+        });
+
+        colorCheckboxes.forEach(function (checkbox) {
+            checkbox.checked = false;
+        });
+
+        sizeCheckboxes.forEach(function (checkbox) {
+            checkbox.checked = false;
+        });
+
+        // Reset the filtering
+        filterProducts();
     }
-  });
-}
 
-// Call the filterProducts function to initialize the filtering
-filterProducts();
-
-
-
-
-// Price Filter
-// Get references to elements
-  const minInput = document.getElementById("min-value");
-  const maxInput = document.getElementById("max-value");
-  const priceRange = document.getElementById("price-range");
-
-  // Initialize the noUiSlider
-  noUiSlider.create(priceRange, {
-    start: [0, 500], // Initial values for the slider
-    range: {
-      min: 0,
-      max: 500
+    // Add an event listener to the "Clear Filters" button
+    const clearFiltersButton = document.getElementById('clear-filters');
+    if (clearFiltersButton) {
+        clearFiltersButton.addEventListener('click', clearFilters);
     }
-  });
-
-  // Attach event listeners to keep inputs in sync with slider
-  priceRange.noUiSlider.on('update', function (values, handle) {
-    if (handle === 0) {
-      minInput.value = parseInt(values[0]);
-    }
-    if (handle === 1) {
-      maxInput.value = parseInt(values[1]);
-    }
-  });
-
-  minInput.addEventListener("input", function () {
-    if (minInput.value === "") {
-      // If the input field is cleared, do not reset to the initialized value
-      priceRange.noUiSlider.set([0, parseInt(maxInput.value)]);
-    } else {
-      priceRange.noUiSlider.set([parseInt(minInput.value), null]);
-    }
-  });
-
-  maxInput.addEventListener("input", function () {
-    if (maxInput.value === "") {
-      // If the input field is cleared, do not reset to the initialized value
-      priceRange.noUiSlider.set([parseInt(minInput.value)]);
-    } else {
-      priceRange.noUiSlider.set([null, parseInt(maxInput.value)]);
-    }
-  });
-
-
-
-
 
 </script>
-
-
 
 
 </body>
